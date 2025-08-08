57002579f4f057eca0a2e8e90d478c62f68ca589
alexandre-s-costa <alexandresc.ufg@gmail.com>
2025-08-08 16:48:45 -0300

    Schema analítico com estratégias de particionamento

--- homework/sql/02_projeto_esquema.sql
+++ homework/sql/02_projeto_esquema.sql
@@ -4,10 +4,7 @@ CREATE SCHEMA IF NOT EXISTS analytics;
 -- Define o schema como padrão
 SET search_path TO analytics;
 
--- Todas as tabelas abaixo conterão os campos: 
--- producaoID, titulo, ano_producao, tipo_ID, nome_pessoa, papel
-
--- `movies` - Todas as produções de filmes 
+-- movies - Todas as produções de filmes 
 CREATE TABLE analytics.movies (
 	producaoID INT,
 	titulo VARCHAR(255),
@@ -18,11 +15,7 @@ CREATE TABLE analytics.movies (
 	PRIMARY KEY (producaoID, ano_producao)
 ) PARTITION BY RANGE (ano_producao);
 
--- Índices úteis
-CREATE INDEX idx_movies_nome_pessoa ON analytics.movies(nome_pessoa);
-CREATE INDEX idx_movies_titulo ON analytics.movies(titulo);
-
--- Partições por década
+-- Estratégia de Particionamento por década
 CREATE TABLE analytics.movies_1980s PARTITION OF analytics.movies
 	FOR VALUES FROM (1980) TO (1990);
 CREATE TABLE analytics.movies_1990s PARTITION OF analytics.movies
@@ -38,7 +31,7 @@ CREATE TABLE analytics.movies_2020s PARTITION OF analytics.movies
 CREATE TABLE analytics.movies_others PARTITION OF analytics.movies
 	DEFAULT;
 
--- `tv_shows` - Todas as produções de séries de TV 
+-- tv_shows - Todas as produções de séries de TV 
 CREATE TABLE analytics.tv_shows (
 	producaoID INT PRIMARY KEY,
 	titulo VARCHAR(255),
@@ -47,10 +40,8 @@ CREATE TABLE analytics.tv_shows (
 	nome_pessoa VARCHAR(255),
 	papel TEXT
 );
-CREATE INDEX idx_tv_shows_nome_pessoa ON analytics.tv_shows(nome_pessoa);
-CREATE INDEX idx_tv_shows_ano ON analytics.tv_shows(ano_producao);
 
--- `video_games` - Todas as produções de videogames 
+-- video_games - Todas as produções de videogames 
 CREATE TABLE analytics.video_games (
 	producaoID INT PRIMARY KEY,
 	titulo VARCHAR(255),
@@ -60,7 +51,7 @@ CREATE TABLE analytics.video_games (
 	papel TEXT
 );
 
--- `documentaries` - Todas as produções de documentários
+-- documentaries - Todas as produções de documentários
 CREATE TABLE analytics.documentaries (
 	producaoID INT PRIMARY KEY,
 	titulo VARCHAR(255),
@@ -69,9 +60,9 @@ CREATE TABLE analytics.documentaries (
 	nome_pessoa VARCHAR(255),
 	papel TEXT
 );
-CREATE INDEX idx_documentaries_nome_pessoa ON analytics.documentaries(nome_pessoa);
 
--- `short_films` - Todas as produções de curtas-metragens 
+
+-- short_films - Todas as produções de curtas-metragens 
 CREATE TABLE analytics.short_films (
 	producaoID INT PRIMARY KEY,
 	titulo VARCHAR(255),
@@ -81,7 +72,7 @@ CREATE TABLE analytics.short_films (
 	papel TEXT
 );
 
--- `music_videos` - Todas as produções de clipes de música 
+-- music_videos - Todas as produções de clipes de música 
 CREATE TABLE analytics.music_videos (
 	producaoID INT PRIMARY KEY,
 	titulo VARCHAR(255),
@@ -91,7 +82,7 @@ CREATE TABLE analytics.music_videos (
 	papel TEXT
 );
 
--- `theater_productions` - Todas as produções teatrais 
+-- theater_productions - Todas as produções teatrais 
 CREATE TABLE analytics.theater_productions (
 	producaoID INT PRIMARY KEY,
 	titulo VARCHAR(255),
@@ -101,7 +92,7 @@ CREATE TABLE analytics.theater_productions (
 	papel TEXT
 );
 
--- `web_series` - Todas as produções de séries web 
+-- web_series - Todas as produções de séries web 
 CREATE TABLE analytics.web_series (
 	producaoID INT PRIMARY KEY,
 	titulo VARCHAR(255),
@@ -111,7 +102,7 @@ CREATE TABLE analytics.web_series (
 	papel TEXT
 );
 
--- `animations` - Todas as produções de animação 
+-- animations - Todas as produções de animação 
 CREATE TABLE analytics.animations (
 	producaoID INT PRIMARY KEY,
 	titulo VARCHAR(255),
@@ -122,3 +113,18 @@ CREATE TABLE analytics.animations (
 );
 
 
+-- Estrategias de indexação
+CREATE INDEX idx_movies_nome_pessoa ON analytics.movies(nome_pessoa);
+CREATE INDEX idx_movies_titulo ON analytics.movies(titulo);
+CREATE INDEX idx_tv_shows_nome_pessoa ON analytics.tv_shows(nome_pessoa);
+CREATE INDEX idx_tv_shows_ano ON analytics.tv_shows(ano_producao);
+CREATE INDEX idx_documentaries_nome_pessoa ON analytics.documentaries(nome_pessoa);
+
+-- Estratégias de particionamento
+
+/*
+PARTITION BY RANGE - usado na linha 16
+Partição por década - linhas 19 a 28
+Partição default - linhas 31 e 32
+
+*/
