42a39bf6c3637bd4b0509c82ff806ef3adb06eb1
Júlio César Prado <juliuscaesar.mat@gmail.com>
2025-08-08 15:57:11 -0300

    Create 03_gerenciamento_usuarios.sql

---
+++ homework/sql/03_gerenciamento_usuarios.sql
@@ -0,0 +1,41 @@
+-- Roles de identidade (USUÁRIOS E SERVIÇOS)
+CREATE ROLE analyst_movies WITH LOGIN PASSWORD 'senha_forte_para_analyst_movies';
+CREATE ROLE analyst_tv WITH LOGIN PASSWORD 'senha_forte_para_analyst_tv';
+CREATE ROLE analyst_games WITH LOGIN PASSWORD 'senha_forte_para_analyst_games';
+CREATE ROLE analyst_all WITH LOGIN PASSWORD 'senha_forte_para_analyst_all';
+CREATE ROLE data_scientist WITH LOGIN PASSWORD 'senha_forte_para_data_scientist';
+
+-- Conceder permissões com GRANT
+GRANT CONNECT ON DATABASE cinetech_productions TO analyst_movies;
+GRANT USAGE ON SCHEMA analytics TO analyst_movies;
+GRANT SELECT ON analytics.movies TO analyst_movies;
+
+-- analyst_tv
+GRANT CONNECT ON DATABASE cinetech_productions TO analyst_tv;
+GRANT USAGE ON SCHEMA analytics TO analyst_tv;
+GRANT SELECT ON analytics.tv_shows TO analyst_tv;
+
+-- analyst_games
+GRANT CONNECT ON DATABASE cinetech_productions TO analyst_games;
+GRANT USAGE ON SCHEMA analytics TO analyst_games;
+GRANT SELECT ON analytics.video_games TO analyst_games;
+
+-- analyst_docs
+GRANT CONNECT ON DATABASE cinetech_productions TO analyst_docs;
+GRANT USAGE ON SCHEMA analytics TO analyst_docs;
+GRANT SELECT ON analytics.documentaries TO analyst_docs;
+
+-- analyst_all
+GRANT CONNECT ON DATABASE cinetech_productions TO analyst_all;
+GRANT USAGE ON SCHEMA analytics TO analyst_all;
+GRANT SELECT ON ALL TABLES IN SCHEMA analytics TO analyst_all;
+
+-- Garante acesso futuro também
+ALTER DEFAULT PRIVILEGES IN SCHEMA analytics
+GRANT SELECT ON TABLES TO analyst_all;
+
+-- data_scientist
+GRANT CONNECT ON DATABASE cinetech_productions TO data_scientist;
+GRANT USAGE ON SCHEMA analytics TO data_scientist;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA analytics TO data_scientist;
+
