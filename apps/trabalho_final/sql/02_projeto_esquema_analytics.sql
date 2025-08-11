4a32c0df6fb99b9c93a1031551e27b444c0a2b88
sudjoao <isudjoao@gmail.com>
2025-08-11 09:39:43 -0300

    chore: Remove sql duplicado

--- apps/trabalho_final/sql/02_projeto_esquema_analytics.sql
+++ apps/trabalho_final/sql/02_projeto_esquema_analytics.sql
@@ -1,27 +1,3 @@
--- Dentro do DB cinetech_productions
--- RAW DATA ----------------------------------------------------
-CREATE TABLE IF NOT EXISTS raw_data.producoes (
-    producao_id INT PRIMARY KEY,
-    titulo      VARCHAR(255) NOT NULL,
-    ano         INT NOT NULL,
-    tipo_id     INT NOT NULL
-);
-CREATE INDEX IF NOT EXISTS idx_producoes_tipo_id ON raw_data.producoes(tipo_id);
-
-CREATE TABLE IF NOT EXISTS raw_data.pessoas (
-    pessoa_id INT PRIMARY KEY,
-    nome      VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS raw_data.equipes (
-    pessoa_id   INT NOT NULL,
-    producao_id INT NOT NULL,
-    papel       TEXT NOT NULL,
-    PRIMARY KEY (pessoa_id, producao_id),
-    FOREIGN KEY (pessoa_id) REFERENCES raw_data.pessoas(pessoa_id),
-    FOREIGN KEY (producao_id) REFERENCES raw_data.producoes(producao_id)
-);
-
 -- ANALYTICS ---------------------------------------------------
 CREATE TABLE IF NOT EXISTS analytics.production_types (
     tipo_id   INT PRIMARY KEY,
